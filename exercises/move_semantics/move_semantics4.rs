7184a247b5e508badc0caf4f5c776d40c84ccccc
a110y <463154249@qq.com>
2023-10-10 17:06:58 +0800

    init

--- exercises/move_semantics/move_semantics4.rs
+++ exercises/move_semantics/move_semantics4.rs
@@ -10,11 +10,11 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let vec0: Vec<i32> = Vec::new();
 
     let mut vec1 = fill_vec(vec0);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", &vec1.len(), &vec1);
 
     vec1.push(88);
 
@@ -22,7 +22,7 @@ fn main() {
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
